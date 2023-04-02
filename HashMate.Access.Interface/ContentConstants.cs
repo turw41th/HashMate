890fc218f1c6bfaaa6a0f8a5a798b9fd296f6f4d
Turwaith <nicolasbaur@turwaith.dev>
2023-04-02 18:24:47 +0200

    Adds Interface for business logic access

---
+++ HashMate.Access.Interface/ContentConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashMate.Access.Interface
+{
+    public class ContentConstants
+    {
+        public enum HashAlgorithms
+        {
+            MD5,
+            SHA1,
+            SHA256,
+            SHA3_256,
+            BLAKE2,
+            WHIRLPOOL,
+            RIPEMD,
+            TIGER,
+            GOST,
+            SIPHASH
+        }
+    }
+}
---
+++ HashMate.Access.Interface/HashEngineAccess.cs
@@ -0,0 +1,9 @@
+﻿namespace HashMate.Access.Interface
+{
+    interface IHashEngineAccess
+    {
+        public bool Initialize(Dictionary<string, string> config);
+
+        public Dictionary<string, string> GetHash(string clearText);
+    }
+}
