53282223a5aca60b17c90c32a359b6cb341dbcb3
Nicolas <nicolas.baur@softart.ch>
2023-04-27 10:13:42 +0200

    Adds properties for hashed text

--- HashMate.Client.UI/ViewModels/TextToHashViewModel.cs
+++ HashMate.Client.UI/ViewModels/TextToHashViewModel.cs
@@ -9,5 +9,24 @@ namespace HashMate.Client.UI.ViewModels
 {
     public class TextToHashViewModel : BaseNotifier
     {
+        private string plainText;
+        private string hashedMD5;
+        private string hashedSHA1;
+        private string hashedSHA2;
+        private string hashedSHA3;
+
+        public string PlainText { get => plainText; set => SetProperty(ref plainText, value); }
+        public string HashedMD5 { get => hashedMD5; set => SetProperty(ref hashedMD5, value); }
+        public string HashedSHA1 { get => hashedSHA1; set => SetProperty(ref hashedSHA1, value); }
+        public string HashedSHA2 { get => hashedSHA2; set => SetProperty(ref hashedSHA2, value); }
+        public string HashedSHA3 { get => hashedSHA3; set => SetProperty(ref hashedSHA3, value); }
+
+
+
+
+        public TextToHashViewModel()
+        {
+
+        }
     }
 }
