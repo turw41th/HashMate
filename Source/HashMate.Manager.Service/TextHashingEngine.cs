6bc6cf1920512dfa6fcdac7682b85f834d29e2d4
Nicolas <nicolas.baur@softart.ch>
2023-05-03 09:27:48 +0200

    Adds engine class that creates the actual hash value

---
+++ Source/HashMate.Manager.Service/TextHashingEngine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace HashMate.Manager.Service
+{
+    internal class TextHashingEngine
+    {
+        public TextHashingEngine() { }
+
+        public static string GetMD5Hash(string input)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    builder.Append(data[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string GetSHA1Hash(string input)
+        {
+            using (SHA1 sha1Hash = SHA1.Create())
+            {
+                byte[] data = sha1Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    builder.Append(data[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string GetSHA256Hash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    builder.Append(data[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
