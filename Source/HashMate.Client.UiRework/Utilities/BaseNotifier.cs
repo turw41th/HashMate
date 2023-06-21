6916bb7826d7ba12a0f75c542027bf8f7a36fb1d
Nicolas <nicolas.baur@softart.ch>
2023-06-21 08:55:37 +0200

    Adds utility classes for mvvm

---
+++ Source/HashMate.Client.UiRework/Utilities/BaseNotifier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HashMate.Client.UiRework.Utilities
+{
+    public abstract class BaseNotifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Source/HashMate.Client.UiRework/Utilities/MyICommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HashMate.Client.UiRework.Utilities
+{
+    public class MyICommand : ICommand
+    {
+        private Action<object> _TargetExecuteMethod;
+
+        private Predicate<object> _TargetCanExecuteMethod;
+
+        public event EventHandler CanExecuteChanged = delegate
+        {
+        };
+
+        public MyICommand(Action<object> executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public MyICommand(Action<object> executeMethod, Predicate<object> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            this.CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_TargetCanExecuteMethod != null)
+            {
+                return _TargetCanExecuteMethod(parameter);
+            }
+
+            if (_TargetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            _TargetExecuteMethod?.Invoke(parameter);
+        }
+    }
+}
